210aef3adb805e7c94a83f65aebd54a32bc42726
AbrantesMar <abs.marcio@gmail.com>
2022-08-29 22:47:08 -0300

    adicionar pontos no mapa com o MKPointAnnotation

--- wantEat/wantEat/Controller/PlacesTableViewController.swift
+++ wantEat/wantEat/Controller/PlacesTableViewController.swift
@@ -11,18 +11,30 @@ class PlacesTableViewController: UITableViewController {
 
     var places: [Place] = []
     let ud = UserDefaults.standard
-    
+    var lbNoPlaces: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loadPlace()
+        lbNoPlaces = UILabel()
+        lbNoPlaces.text = "Cadastre os locais que deseja conhecer \n clicando no botão + acima"
+        lbNoPlaces.textAlignment = .center
+        lbNoPlaces.numberOfLines = 0
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier! != "mapSegue" {
             let viewController = segue.destination as! PlacesFinderViewController
             viewController.delegate = self
+        } else {
+            let viewController = segue.destination as! MapViewController
+            switch sender {
+                case let place as Place:
+                    viewController.places = [place]
+                default:
+                    viewController.places = places
+            }
         }
     }
     
@@ -44,9 +56,19 @@ class PlacesTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    @objc func showAll() {
+        performSegue(withIdentifier: "mapSegue", sender: nil)
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if places.count > 0 {
+            let btShowAll = UIBarButtonItem(title: "Mostrar todos no mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowAll
+            tableView.backgroundView = nil
+        } else {
+            navigationItem.leftBarButtonItem = nil
+            tableView.backgroundView = lbNoPlaces
+        }
         return places.count
     }
 
@@ -57,16 +79,19 @@ class PlacesTableViewController: UITableViewController {
         cell.textLabel?.text = place.name
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "mapSegue", sender: place)
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            places.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            savePlace()
+        }
+    }
 
 }
 
